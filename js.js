56e91418422317edc2e28823a36e4aa89da84f57
Roy Canseco <roy.canseco@gmail.com>
2025-08-07 12:01:24 +0800

    did NG1 for Q1

---
+++ js.js
@@ -0,0 +1,15 @@
+         
+         var $x=_y=z=5
+
+         GREETING = "Hi, Pisay </br>"
+         document.write(GREETING)
+         document.write("The value of the variable's content x is: " +($x +0) +"<br>")
+         document.write(' The "value" of variable\'s content y is: ' +(_y +0) +"<br>")
+         document.write(`The value of variable z is: ${z +0} <br>`)
+
+         console.log("The value of variable x is: " +($x +0))
+         console.log("The value of variable y is: " +(_y +0))
+         console.log("The value of variable z is: " +(z +0 ) + " " + (6.022E23 *2))
+
+
+         
